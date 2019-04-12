0b28a15816036eff1cd781063e3246544f1e3681
Mirko Rosenthal <mirko91194@googlemail.com>
2019-04-12 03:04:18 +0200

    Adds time tracking features

--- APGG/main.cpp
+++ APGG/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <chrono>
 #include "World.h"
 
-
+typedef std::chrono::high_resolution_clock HighResClock;
+typedef std::chrono::milliseconds ms;
+typedef std::chrono::duration<double> fsec;
 
 int main() {	
 	World myCoolWorld;
 	
+    auto t_start = HighResClock::now();
+
+
 	while (myCoolWorld.m_generation < 10000)
 	{
 		std::cout << "Num of Gen: " << myCoolWorld.m_generation;
@@ -15,7 +21,12 @@ int main() {
 
 
 	}
-	std::cout << "Done";
-	getchar();
+
+    auto t_end = HighResClock::now();
+    fsec fs = t_end - t_start;
+    ms timeDelta = std::chrono::duration_cast<ms>(fs);
+
+    std::cout << "Done (took " << timeDelta.count() << " ms)";
+    getchar();
 	return 0;
 }
