90acd2cfd27d4238686db2dfa9b67b414d13b1c0
Mirko Rosenthal <mirko91194@googlemail.com>
2020-02-14 15:14:15 +0100

    Fixes wrong debug message in ProportionateRepopulator.cpp

--- APGG/optimizer/repopulators/ProportionateRepopulator.cpp
+++ APGG/optimizer/repopulators/ProportionateRepopulator.cpp
@@ -48,47 +48,9 @@ namespace APGG {
           //  deadOrganism.ID = grid.getID();
 
 
-            DEBUG_MSG("Repopulator: created offspring " + deadOrganism.get().getDebugString());
-            DEBUG_MSG("Repopulator: from parent " + rParent.get().getDebugString());
+            DEBUG_MSG("Repopulator: created offspring " + deadOrganism.getDebugString());
+            DEBUG_MSG("Repopulator: from parent " + grid[parentOrganismIndex].getDebugString());
         }
-
-   //     rOrganism rParent = grid->getRandomOrganism(selection);
-
-   //     for (rOrganism& deadOrganism : selection)
-   //     {
-			//float payoff = std::numeric_limits<float>::min();
-			//
-			//while (payoff <= getRandomFloat()) {
-   //             rParent = grid->getRandomOrganism(selection);
-
-   //             if (min == max || payoff == 0) {
-   //                 break;
-   //             }
-
-			//	payoff = rParent.get().getNormalizedPayoff(min, max);
-			//}
-
-   //         pOrganism ptr = deadOrganism.get().getPtr();
-
-   //         deadOrganism.get().m_parent = rParent.get().getPtr();
-   //         deadOrganism.get().m_parent->addChild(ptr);
-   //         ptr.reset();
-
-
-			////Copy genomes && reset organism
-   //         deadOrganism.get().m_genomes = rParent.get().m_genomes;
-   //         deadOrganism.get().m_status = STATUS_ORIGINAL;
-   //         deadOrganism.get().m_mutated = false;
-   //         deadOrganism.get().clearChildren();
-   //         deadOrganism.get().m_generation = grid->getGeneration();
-
-			////Assign new ID
-			//deadOrganism.get().ID = grid->getID();
-
-
-   //         DEBUG_MSG("Repopulator: created offspring " + deadOrganism.get().getDebugString());
-   //         DEBUG_MSG("Repopulator: from parent " + rParent.get().getDebugString());
-    //    }
     }
 	void ProportionateRepupoluator::configure(Config& config)
 	{
