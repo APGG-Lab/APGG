7ab057b041132f15513b220b990ed4517851d52d
Mirko Rosenthal <mirko91194@googlemail.com>
2020-05-06 17:47:10 +0200

    Removes semicolons from python script

--- APGG/buildConfig.py
+++ APGG/buildConfig.py
@@ -1,7 +1,7 @@
 import csv
 
 def main():
-    num_experiments = 30; #How often do you want to repeat the experiment
+    num_experiments = 30 #How often do you want to repeat the experiment
     baseEntry = {
         'numGenerations' :  450000,
         'eliminationCount' : 100,
@@ -24,59 +24,59 @@ def main():
         'timeToFolder' : 0,
     }
 
-    addIteration('synergyFactor', 0, 6, 0.5); #From, To, StepSize
-    addIteration('mutatortype', Mutator['Random'], Mutator['RandomThreshold'], 1); #From, To, StepSize
+    addIteration('synergyFactor', 0, 6, 0.5) #From, To, StepSize
+    addIteration('mutatortype', Mutator['Random'], Mutator['RandomThreshold'], 1) #From, To, StepSize
 
-    buildConfig("configs.csv", num_experiments, baseEntry);
+    buildConfig("configs.csv", num_experiments, baseEntry)
 
 #=======================================================
 
-Grid = {'Default': 0, 'Spatial' : 1};
-PayoffCalculator = {'Default' : 0, 'Sptatial': 1};
-Selector = {'Elite' : 0, 'Random' : 1};
-Mutator = {'Random' : 0, 'RandomThreshold': 1};
-Repopulator = {'Proportionate' : 0, 'Random' : 1, 'Spatial' : 2};
+Grid = {'Default': 0, 'Spatial' : 1}
+PayoffCalculator = {'Default' : 0, 'Sptatial': 1}
+Selector = {'Elite' : 0, 'Random' : 1}
+Mutator = {'Random' : 0, 'RandomThreshold': 1}
+Repopulator = {'Proportionate' : 0, 'Random' : 1, 'Spatial' : 2}
 
 
 def addIteration(index, startVal, endVal, stepSize):
     """Adds iteration to array of iterations"""
 
-    iteration =  {'key': index, 'from' : startVal, 'to' : endVal, 'stepSize' : stepSize};
-    iterations.append(iteration);
-    return iteration;
+    iteration =  {'key': index, 'from' : startVal, 'to' : endVal, 'stepSize' : stepSize}
+    iterations.append(iteration)
+    return iteration
 
-iterations = [];
+iterations = []
 
 def buildConfig(configName, num_experiments, baseEntry):
     """Generates a config and writes it to configName"""
 
-    csvData = []; #CSV-Data Array
-    num_experiments_counter = 0;
+    csvData = [] #CSV-Data Array
+    num_experiments_counter = 0
 
-    recursiveIteration(0, baseEntry.copy(), csvData, num_experiments);
+    recursiveIteration(0, baseEntry.copy(), csvData, num_experiments)
 
     writeConfig(configName, csvData)
-    print("Wrote " + str(len(csvData)) + " experiments to \"" + configName + "\"");
+    print("Wrote " + str(len(csvData)) + " experiments to \"" + configName + "\"")
 
 def recursiveIteration(iterationIndex, baseEntry, csvData, num_experiments):
     """Generates experiments recursively"""
 
     if iterationIndex >= len(iterations): #reached end of iterations
         for i in range(num_experiments):
-            experimentCopy = baseEntry.copy();
-            experimentCopy['logSuffix'] += "-exp" + str(i);
-            csvData.append(list(experimentCopy.values()));
-            print(experimentCopy['logSuffix']);
-        return;
+            experimentCopy = baseEntry.copy()
+            experimentCopy['logSuffix'] += "-exp" + str(i)
+            csvData.append(list(experimentCopy.values()))
+            print(experimentCopy['logSuffix'])
+        return
 
-    iteration = iterations[iterationIndex];
+    iteration = iterations[iterationIndex]
 
-    counter = iteration['from'];
+    counter = iteration['from']
     while counter <= iteration['to']:
-        baseCopy = baseEntry.copy();
-        baseCopy[iteration['key']] = counter;
-        baseCopy['logSuffix'] += "-" + iteration['key'] + str(counter);
-        counter += iteration['stepSize'];
+        baseCopy = baseEntry.copy()
+        baseCopy[iteration['key']] = counter
+        baseCopy['logSuffix'] += "-" + iteration['key'] + str(counter)
+        counter += iteration['stepSize']
         recursiveIteration(iterationIndex +1, baseCopy, csvData, num_experiments)
 
 def writeConfig(name, data):
