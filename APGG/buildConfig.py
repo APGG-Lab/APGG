b88921c4a72c8aa18f514676daf6729d17ee228c
Mirko Rosenthal <mirko91194@googlemail.com>
2020-05-06 17:43:04 +0200

    Updates config generator script

--- APGG/buildConfig.py
+++ APGG/buildConfig.py
@@ -1,57 +1,92 @@
 import csv
 
+def main():
+    num_experiments = 30; #How often do you want to repeat the experiment
+    baseEntry = {
+        'numGenerations' :  450000,
+        'eliminationCount' : 100,
+        'groupSize' : 8,
+        'width' : 32,
+        'height' : 32,
+        'cooperateCost' : 1,
+        'synergyFactor' : 1,
+        'punishmentcost' : 0.0,
+        'punishmentfine' : 0.0,
+        'gridtype' : Grid['Default'],
+        'payofftype' : PayoffCalculator['Default'],
+        'selectortype' : Selector['Random'],
+        'repopulationtype' : Repopulator['Proportionate'],
+        'mutatortype' : Mutator['Random'],
+        'mutationRate' : 0.01,
+        'folderName' : 'fig6-exp',
+        'logSuffix': 'test1_r1',
+        'timeToFile' : 1,
+        'timeToFolder' : 0,
+    }
 
-csvData = [
-    ["numGenerations", "eliminationCount","groupSize",
-     "width","height",
-     "cooperateCost","synergyFactor",
-     "punishmentCost","punishmentFine",
-     "matchupType","selectorType","repopulationType","gridType","payoffType","mutatorType",
-     "mutationRate",
-     "showAllGenerations",
-     "archiveData","visualize",
-     "folderName","logSuffix","timeToFile","timeToFolder",
-     "consoleOutExponent"]
-]
+    addIteration('synergyFactor', 0, 6, 0.5); #From, To, StepSize
+    addIteration('mutatortype', Mutator['Random'], Mutator['RandomThreshold'], 1); #From, To, StepSize
 
-baseEntry = {
-    'numGenerations' :  450000,
-    'eliminationCount' : 100,
-    'groupSize' : 8,
-    'width' : 32,
-    'height' : 32,
-    'cooperateCost' : 1,
-    'synergyFactor' : 1,
-    'punishmentcost' : 0.0,
-    'punishmentfine' : 0.0,
-    'matchuptype': 0, 'selectortype' : 1, 'repopulationtype' : 1, 'gridtype' : 0, 'payofftype' : 0, 'mutatortype' : 0,
-    'mutationRate' : 0.01,
-    'showallgenerations' : 0,
-    'archiveData' : 1,
-    'visualize' : 0,
-    'folderName' : 'fig6-exp',
-    'logSuffix': 'test1_r1',
-    'timeToFile' : 1,
-    'timeToFolder' : 0,
-    'consoleOutExponent' : 10
-}
+    buildConfig("configs.csv", num_experiments, baseEntry);
 
-i = 1.0
-while i < 6.5:
-    for j in range(0, 30):
-        copy = baseEntry
-        copy["synergyFactor"] = i
-        copy["logSuffix"] = "exp" + str(j) + "-syn" + str(i)
+#=======================================================
 
-        for k in range(0,2):
-            copy["mutatortype"] = k
-            copy['folderName'] = 'fig6-mut' + str(k) + "-zeroPunish"
-            
-            arrayLine = list(copy.values())
-            csvData.append(arrayLine)
-    i += 0.5
+Grid = {'Default': 0, 'Spatial' : 1};
+PayoffCalculator = {'Default' : 0, 'Sptatial': 1};
+Selector = {'Elite' : 0, 'Random' : 1};
+Mutator = {'Random' : 0, 'RandomThreshold': 1};
+Repopulator = {'Proportionate' : 0, 'Random' : 1, 'Spatial' : 2};
 
-with open('configs.csv', 'w', newline='') as csvFile:
-    writer = csv.writer(csvFile, delimiter=";", )
-    writer.writerows(csvData)
-csvFile.close()
+
+def addIteration(index, startVal, endVal, stepSize):
+    """Adds iteration to array of iterations"""
+
+    iteration =  {'key': index, 'from' : startVal, 'to' : endVal, 'stepSize' : stepSize};
+    iterations.append(iteration);
+    return iteration;
+
+iterations = [];
+
+def buildConfig(configName, num_experiments, baseEntry):
+    """Generates a config and writes it to configName"""
+
+    csvData = []; #CSV-Data Array
+    num_experiments_counter = 0;
+
+    recursiveIteration(0, baseEntry.copy(), csvData, num_experiments);
+
+    writeConfig(configName, csvData)
+    print("Wrote " + str(len(csvData)) + " experiments to \"" + configName + "\"");
+
+def recursiveIteration(iterationIndex, baseEntry, csvData, num_experiments):
+    """Generates experiments recursively"""
+
+    if iterationIndex >= len(iterations): #reached end of iterations
+        for i in range(num_experiments):
+            experimentCopy = baseEntry.copy();
+            experimentCopy['logSuffix'] += "-exp" + str(i);
+            csvData.append(list(experimentCopy.values()));
+            print(experimentCopy['logSuffix']);
+        return;
+
+    iteration = iterations[iterationIndex];
+
+    counter = iteration['from'];
+    while counter <= iteration['to']:
+        baseCopy = baseEntry.copy();
+        baseCopy[iteration['key']] = counter;
+        baseCopy['logSuffix'] += "-" + iteration['key'] + str(counter);
+        counter += iteration['stepSize'];
+        recursiveIteration(iterationIndex +1, baseCopy, csvData, num_experiments)
+
+def writeConfig(name, data):
+    """Writes csv to filesystem"""
+
+    with open(name, 'w', newline='') as csvFile:
+        writer = csv.writer(csvFile, delimiter=";", )
+        writer.writerows(data)
+        csvFile.close()
+
+#Neat trick to define methods AFTER using them
+if __name__ == '__main__':
+    main()
