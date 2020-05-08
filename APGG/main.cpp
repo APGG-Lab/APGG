050336f01e0d5c12d81ab077fdc1ed156a60901b
Mirko Rosenthal <mirko91194@googlemail.com>
2020-05-08 20:45:20 +0200

    Adds startparameter for the number of threads

--- APGG/main.cpp
+++ APGG/main.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <vector>
 #include <future>
+#include <algorithm>
 
 #include "World.h"
 #include "ConfigParserCSV.h"
@@ -22,77 +23,80 @@ void RunExperiment(Config config)
 	}
 
 	myCoolWorld.Fini();
-
-
-	if (stoi(config.getValue("visualize", "0")) && false) {
-		//todo find better way to open the python script automatically
-		system("python3 Visualize.py");
-	}
 }
 
-int main() {
+int main(int argc, char * argv[]) {
 	auto configs = ConfigParserCSV::parseConfigs("configs.csv");
     std::cout << "[APGG Init] loading " << configs.size() << " experiments" << std::endl;
 
-	RunExperiment(configs[0]);
+	int threadCount = 1;
+	if (argc == 2) {
+		threadCount = std::stoi(argv[1]);
+		if (threadCount < 1) {
+			std::cout << "[APGG Error] Invalid number of threads. Threadcount must be >=1 (actual value: " << std::to_string(threadCount) << ")";
+			std::cin.get();
+			std::quick_exit(1);
+		}
+	}
 
-	//using namespace std::chrono_literals;
-	////TODO: make configurable
-	//unsigned int concurentNumberThreads = 1;
-	//if (concurentNumberThreads > configs.size())
-	//{
-	//	concurentNumberThreads = configs.size();
-	//}
+	using namespace std::chrono_literals;
+	//TODO: make configurable
+	unsigned int concurentNumberThreads = static_cast<unsigned int>(threadCount);
+	if (concurentNumberThreads > static_cast<unsigned int>(configs.size()))
+	{
+		concurentNumberThreads = static_cast<unsigned int>(configs.size());
+	}
 
-	//std::vector<std::future<void>> threadFutures;
+	std::cout << "[APGG Init] Using " << std::to_string(concurentNumberThreads) << " threads" << std::endl;
 
-	//for (int i = 0; i < concurentNumberThreads; i++)
-	//{
-	//	threadFutures.push_back(std::async(std::launch::async, RunExperiment, configs[i]));
-	//}
+	std::vector<std::future<void>> threadFutures;
 
-	//unsigned int nextThread = concurentNumberThreads;
-	//unsigned int numberTasks = configs.size();
-	//bool remainingTasks = numberTasks > concurentNumberThreads;
+	for (unsigned int i = 0; i < concurentNumberThreads; i++)
+	{
+		threadFutures.push_back(std::async(std::launch::async, RunExperiment, configs[i]));
+	}
 
-	//while (remainingTasks)
-	//{
-	//	for (auto& future : threadFutures)
-	//	{
-	//		if (future.wait_for(0ms) == std::future_status::ready)
-	//		{
-	//			//start new thread in case there is still work to be done
-	//			if (nextThread < (numberTasks))
-	//			{
-	//				future = std::async(std::launch::async, RunExperiment, configs[nextThread]);
-	//				nextThread++;
-	//			}
-	//			else
-	//			{
-	//				remainingTasks = false;
-	//				break;
-	//			}
+	unsigned int nextThread = concurentNumberThreads;
+	unsigned int numberTasks = static_cast<unsigned int>(configs.size());
+	bool remainingTasks = numberTasks > concurentNumberThreads;
 
-	//		}
-	//	}
-	//}
+	while (remainingTasks)
+	{
+		for (auto& future : threadFutures)
+		{
+			if (future.wait_for(0ms) == std::future_status::ready)
+			{
+				//start new thread in case there is still work to be done
+				if (nextThread < (numberTasks))
+				{
+					future = std::async(std::launch::async, RunExperiment, configs[nextThread]);
+					nextThread++;
+				}
+				else
+				{
+					remainingTasks = false;
+					break;
+				}
 
-	////wait for all threads to be finished before the main thread closes
-	//bool allThreadsFinished = false;
-	//while (!allThreadsFinished)
-	//{
-	//	allThreadsFinished = true;
-	//	for (auto& future : threadFutures)
-	//	{
-	//		if (future.wait_for(0ms) != std::future_status::ready)
-	//		{
-	//			allThreadsFinished = false;
-	//			break;
-	//		}
-	//	}
-	//}
+			}
+		}
+	}
 
-	getchar();
+	//wait for all threads to be finished before the main thread closes
+	bool allThreadsFinished = false;
+	while (!allThreadsFinished)
+	{
+		allThreadsFinished = true;
+		for (auto& future : threadFutures)
+		{
+			if (future.wait_for(0ms) != std::future_status::ready)
+			{
+				allThreadsFinished = false;
+				break;
+			}
+		}
+	}
+
 	return 0;
 }
 
